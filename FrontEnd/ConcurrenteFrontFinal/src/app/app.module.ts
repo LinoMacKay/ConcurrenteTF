b268b5450df3e0af5ea4562b9697674526b73bc9
Lino <u201819681@upc.edu.pe>
2021-10-28 23:48:35 -0500

    L.M - Adding Home Page and Appbar

--- FrontEnd/ConcurrenteFrontFinal/src/app/app.module.ts
+++ FrontEnd/ConcurrenteFrontFinal/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
+import { AppbarComponent } from './shared/appbar/appbar.component';
+import { PredictionComponent } from './core/prediction/prediction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AppbarComponent,
+    PredictionComponent
   ],
   imports: [
     BrowserModule,
